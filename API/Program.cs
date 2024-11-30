b837caa2cc891a7294b15b1fb25016cbbe357e28
Nahuel Piñeyro <npineyro@estudiante.ceibal.edu.uy>
2024-11-30 15:29:12 -0300

    added some changes

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
